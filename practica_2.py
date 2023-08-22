6325821e2c8a5506bb627ae05d1f7210321f9b5d
Athansya <atoriz98@outlook.com>
2023-08-22 00:29:25 -0600

    Finished practica 2

--- practica_2.py
+++ practica_2.py
@@ -16,6 +16,7 @@ Description:
 #                                  BIBLIOTECAS                                 #
 # ---------------------------------------------------------------------------- #
 import funciones_mqtt_utils as mqtt
+from random import randint
 import time
 
 # ---------------------------------------------------------------------------- #
@@ -23,74 +24,146 @@ import time
 # ---------------------------------------------------------------------------- #
 BROKER = "mqtt-dashboard.com"
 PORT = 1883
-TOPIC = "iotunam/"
-CLIENT_NAMES = ["A", "B", "C", "D"]
+TOPIC = "testeo_practica_2/"
+CLIENT_NAMES = ["X", "Y", "Z"]
 CLIENT_IDS = [f"Client-{name}" for name in CLIENT_NAMES]
 MESSAGE_LISTS = [[] for _ in CLIENT_NAMES]  # Una lista por cliente
 
 if __name__ == "__main__":
-    # Creamos lista de objetos
-    clients_obj_list = [
-        mqtt.connect_mqtt(client_id=client, broker=BROKER, port=PORT)
-        for client in CLIENT_IDS
+    # Creamos dict de objetos
+    clients = [
+        mqtt.connect_mqtt(
+            client_id=client_id,
+            broker=BROKER,
+            port=PORT
+        ) for client_id in CLIENT_IDS
     ]
 
-    # Suscribimos clientes a topicos
-    print("SUBSCRIBING CLIENTS")
-    for index, client in enumerate(clients_obj_list):
+    # START
+    print("------- STARTING CONNECTIONS -------")
+    for client in clients:
         client.loop_start()
         time.sleep(1)
-        if index == 0:  # Si soy el primer cliente
-            topic_sub = TOPIC + CLIENT_NAMES[-1]
-            print(f"{index=}")
-            print(f"{topic_sub=}")
-            message = "Telefono descompuesto!"
-            mqtt.subscribe(client, CLIENT_IDS[index], topic=topic_sub, message_list=MESSAGE_LISTS[index])
+        print(40 * '-')
+
+    # SUBSCRIBE
+    print("------- SUBSCRIBING TO TOPICS -------")
+    for index, client in enumerate(clients):
+        if index == 0:
+            topic = TOPIC + CLIENT_NAMES[-1]
         else:
-            if index < len(CLIENT_NAMES):
-                topic_sub = TOPIC + CLIENT_NAMES[index - 1]
-            print(f"{index=}")
-            print(f"{topic_sub=}")
-            mqtt.subscribe(client, CLIENT_IDS[index], topic=topic_sub, message_list=MESSAGE_LISTS[index])
-            time.sleep(1)
-        time.sleep(1)
-        print(50 * '-')
+            topic = TOPIC + CLIENT_NAMES[index - 1]
 
-    # Publicamos mensajes en topicos
-    print("PUBLISHING MESSAGES")
-    for index, client in enumerate(clients_obj_list):
-        client.loop_start()
+        mqtt.subscribe(
+            client=client,
+            client_id=CLIENT_IDS[index],
+            topic=topic,
+            message_list=MESSAGE_LISTS[index]
+        )
+        print(f"{CLIENT_IDS[index]} subscribed to {topic}")
         time.sleep(1)
-        if index == 0:  # Si soy el primer cliente
-            topic_pub = TOPIC + CLIENT_NAMES[1]
-            print(f"{index=}")
-            print(f"{topic_pub=}")
-            message = "Telefono descompuesto!"
-            mqtt.publish(client, CLIENT_IDS[index], topic_pub, message, qos=1)
-            time.sleep(1)
+        print(40 * '-')
+        
+    # PUBLISHING
+    print("------- PUBLISHING IN TOPICS -------")
+    for index, client in enumerate(clients):
+        topic = TOPIC + CLIENT_NAMES[index]
+        if index == 0:
+            message = "Telefono descompuesto..."
+            mqtt.publish(
+                client=client,
+                client_id=CLIENT_IDS[index],
+                topic=topic,
+                msg=message,
+                qos=1
+            )
         else:
-            if index < len(CLIENT_NAMES) - 1:
-                topic_pub = TOPIC + CLIENT_NAMES[index + 1]
-            else:
-                topic_pub = TOPIC + CLIENT_NAMES[0]
-            print(f"{index=}")
-            print(f"{topic_pub=}")
-            mqtt.publish(client, CLIENT_IDS[index], topic_pub, message, qos=1)
-            time.sleep(1)
+            mqtt.publish(
+                client=client,
+                client_id=CLIENT_IDS[index],
+                topic=topic,
+                msg=MESSAGE_LISTS[index][0],
+                qos=1
+            )
         time.sleep(1)
-        print(50 * '-')
+        print(40 * '-')
 
+    # CLOSE
+    for client in clients:
+        client.loop_stop()
+        client.disconnect()
+        
+    # SHOW MESSAGES
+    for index, name in enumerate(CLIENT_NAMES):
+        print(f"Messages received by {name}:")
+        mqtt.show_received_messages(MESSAGE_LISTS[index])
+        
 
-    # Mostramos mensajes recibidos por cada cliente
-    for index, msg_list in enumerate(MESSAGE_LISTS):
-        time.sleep(1)
-        print(f"Mensajes recibidos por {CLIENT_NAMES[index]}")
-        mqtt.show_received_messages(msg_list)
+    # clients = {
+        # name:{
+            # "client": mqtt.connect_mqtt(
+                # client_id=client_id,
+                # broker=BROKER,
+                # port=PORT),
+            # "ID": client_id,
+            # "msgs": []
+        # } for name, client_id in zip(CLIENT_NAMES, CLIENT_IDS)
+    # }
+    # print(clients)
+# 
+    # for index, key in enumerate(clients.keys()):
+        # clients[key]["client"].loop_start()
 
-    # Cerramos las conexiones
-    for client in clients_obj_list:
-        client.loop_stop
+    # Subscribing clients
+    # print("--- CONNECTED AND SUBSCRIBED ---")
+    # for index, key in enumerate(clients.keys()):
+        # clients[key]["client"].loop_start()
+        # if key == "A":
+            # topic = TOPIC + CLIENT_NAMES[-1]
+        # else:
+            # topic = TOPIC + CLIENT_NAMES[index - 1]
+# 
+        # mqtt.subscribe(
+            # client=clients[key]["client"],
+            # client_id=clients[key]["ID"],
+            # topic= TOPIC + CLIENT_NAMES[-1],
+            # message_list=clients[key]["msgs"]
+        # )
+        # time.sleep(1)
+        # print(f"{clients[key]['ID']} - Subscribed to: {topic}")
+        # print(40 * '-')
+# 
+    # Messaging
+    # for index, key in enumerate(clients.keys()):
+        # if key == "A":
+            # message = "Telefono descompuesto!"
+            # mqtt.publish(
+                # client=clients[key]["client"],
+                # client_id=clients[key]["ID"],
+                # topic=TOPIC + key,
+                # msg=message,
+                # qos=1,
+                # repetitions=3
+            # )
+            # time.sleep(3)
+        # else:
+            # mqtt.publish(
+                # client=clients[key]["client"],
+                # client_id=clients[key]["ID"],
+                # topic=TOPIC + key,
+                # msg=clients[key]["msgs"][0],
+                # qos=1
+            # )
+# 
+    # clients["A"]["client"].loop_start()
+    # mqtt.subscribe(
+        # client=clients["A"]["client"],
+        # client_id=clients["A"]["ID"],
+        # topic= TOPIC + list(clients.keys())[-1],
+        # message_list=clients["A"]["msgs"]
+    # )
 
+    # for client in clients_obj_list:
+        # client.loop_stop()
 
-
-
+    # mqtt.show_received_messages(MESSAGE_LISTS)
